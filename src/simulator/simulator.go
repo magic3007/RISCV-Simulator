da1911340a8c74eb4aba300ce880b96d633df92c
magic3007 <maijing3007@qq.com>
2020-03-18 00:37:22 +0800

    [simulator] add "Single Step"

--- src/simulator/simulator.go
+++ src/simulator/simulator.go
@@ -2,15 +2,21 @@ package simulator
 
 import (
 	"debug/elf"
+	"errors"
+	"isa"
+	"log"
 	"memory"
 	"register"
 	"utils"
 )
 
+var STACK_TOP uint64 = 0x7fffff_fff000
+var STACK_SIZE uint64 = 4096
+
 type Simulator struct {
-	m  memory.Memory64
-	rh register.Heap64
-	pc uint64
+	M       memory.Memory64
+	R       register.Heap64
+	PC      uint64
 	elfFile *elf.File
 }
 
@@ -21,15 +27,57 @@ func NewSimulator(elfFilePath string) *Simulator {
 }
 
 func (sim *Simulator) LoadMemory() {
-	for _, prog := range sim.elfFile.Progs{
+	file := sim.elfFile
+	for _, prog := range file.Progs{
 		if prog.Type == elf.PT_LOAD{
 			vaddr, memsz, filesz := prog.Vaddr, prog.Memsz, prog.Filesz
 			buf := make([]byte, filesz)
 			_, err :=  prog.ReadAt(buf, 0)
 			utils.PANIC_CHECK(err)
-			sim.m.Alloc(vaddr, memsz)
-			sim.m.StoreBytes(vaddr, filesz, buf)
+			sim.M.Alloc(vaddr, memsz)
+			sim.M.StoreBytes(vaddr, filesz, buf)
 		}
 	}
-	sim.pc = sim.elfFile.Entry
+
+	// Allocate stack memory and set stack pointer
+	sim.M.Alloc(STACK_TOP - STACK_SIZE, STACK_SIZE)
+	sim.R.StoreByName("sp", STACK_TOP)
+
+	//! Magic: start from |main| function instead of the entry in elf file
+	symbols, _  := file.Symbols()
+	found := false
+	for _, symbol := range symbols {
+		if symbol.Name == "main"{
+			sim.PC = symbol.Value
+			found = true
+			break
+		}
+	}
+	if found == false{
+		log.Fatalln("Can not find the symbol named \"main\"");
+	}
+}
+
+func Match(code uint32) (item *Action, err error) {
+	for _, item := range ActionSet {
+		if item.Pred(code) == true {
+			return &item, nil
+		}
+	}
+	return nil, errors.New("unknown instruction format")
+}
+
+func (sim *Simulator) SingleStep() *Action {
+	m := sim.M
+	code := m.LoadU32(sim.PC)
+	if isa.InstructionLength(code) != 32{
+		log.Panicln("Only support 32-bit instruction")
+	}
+	item, err := Match(code)
+	if err != nil {
+		log.Fatalf(" 0x%08x: %v", code, err)
+	}
+	inst := item.Inst(code)
+	item.Exec(sim, inst)
+	return item
 }
--- src/simulator/simulator_test.go
+++ src/simulator/simulator_test.go
@@ -13,11 +13,12 @@ func TestLoadElf(t *testing.T) {
 		relativePath string
 		expected uint32
 	}{
-		{"add", "/../test_data/add.out", 0x00002197},
-		{"double-float", "/../test_data/double-float.out", 0x00002197},
-		{"mul-div", "/../test_data/mul-div.out", 0x00002197},
-		{"qsort", "/../test_data/qsort.out", 0x00002197},
-		{"simple-function", "/../test_data/simple-function.out", 0x00002197},
+		{"add", "/../test_data/add.out", 0xfe010113},
+		{"double-float", "/../test_data/double-float.out", 0xfe010113},
+		{"mul-div", "/../test_data/mul-div.out", 0xfe010113},
+		{"n!", "/../test_data/n!.out", 0xff010113},
+		{"qsort", "/../test_data/qsort.out", 0xfe010113},
+		{"simple-function", "/../test_data/simple-function.out", 0xff010113},
 	}
 	log.SetFlags(log.Llongfile | log.LstdFlags)
 	for _, test := range tests {
@@ -25,7 +26,31 @@ func TestLoadElf(t *testing.T) {
 			filepath := utils.GetCurrentPath() + test.relativePath
 			sim := NewSimulator(filepath)
 			sim.LoadMemory()
-			assert.Equal(t, test.expected, sim.m.LoadU32(sim.pc))
+			assert.Equal(t, test.expected, sim.M.LoadU32(sim.PC))
+		})
+	}
+}
+
+func TestSingleStep(t *testing.T) {
+	tests := []struct {
+		name string
+		relativePath string
+		expected []string
+	}{
+		{"add", "/../test_data/add.out",
+			[]string{"addi", "sd", "addi", "sw", "sw"},
+			},
+	}
+	log.SetFlags(log.Llongfile | log.LstdFlags)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			filepath := utils.GetCurrentPath() + test.relativePath
+			sim := NewSimulator(filepath)
+			sim.LoadMemory()
+			for _, expectInst := range test.expected{
+				item := sim.SingleStep()
+				assert.Equal(t, expectInst, item.Name)
+			}
 		})
 	}
 }
