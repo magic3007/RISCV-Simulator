ced0c0e78c18d25dfbae13def70782eaf2c42154
magic3007 <maijing3007@qq.com>
2020-03-22 23:01:15 +0800

    [utils] add "Filter"

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -58,3 +58,15 @@ func GetCurrentPath() string{
 	_, filename, _, _ := runtime.Caller(1)
     return path.Dir(filename)
 }
+
+func Filter(arr interface{}, cond func(interface{}) bool) interface{}{
+	contentType := reflect.TypeOf(arr)
+	contentValue := reflect.ValueOf(arr)
+	newContent := reflect.MakeSlice(contentType, 0, 0)
+	for i:= 0; i < contentValue.Len(); i++ {
+		if content := contentValue.Index(i); cond(content.Interface()){
+			newContent = reflect.Append(newContent, content)
+		}
+	}
+	return newContent.Interface()
+}
